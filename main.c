44f5d869f2c04cc7c7cac59fa9fc3bfde3861259
govindgcoder <govind.g.coder@gmail.com>
2026-05-21 17:10:34 +0000

    started assembler - added opcode definitions

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include<stdbool.h>
 #include<stdint.h>
+#include<string.h>
+#include<stdlib.h>
 
 typedef struct CPU {
   uint8_t PC; // program counter
@@ -15,8 +17,42 @@ typedef struct CPU {
   uint8_t stk; // stack pointer
 } CPU;
 
-// no operand NAND means NANDing the accumulator, no operand ST can serve as stop
+void main()
+{
+  char program[256][10];
 
-// I'm thinking of commands: LD, ST, LDI, BRZ, NAND, ADD, PUSH, POP
+  int i = 0;
+  while (program[i]!=NULL && i<256) {
+    char *p = program[i];
+    char ir[];
+    int j = 0;
+    while(*p!=' '){
+      ir[j]=*p;
+      *p+=1;j++;
+    }
+    uint8_t opcode = 0;
+    if(strcmp(ir,"ADD")){
+    } else if(strcmp(ir,"SUB")){
+      opcode = opcode | 0x1;
+    } else if(strcmp(ir,"NAND")){
+      opcode = opcode | 0x2;
+    } else if(strcmp(ir,"LD")){
+      opcode = opcode | 0x3;
+    } else if(strcmp(ir,"ST")){
+      opcode = opcode | 0x4;
+    } else if(strcmp(ir,"PUSH")){
+      opcode = opcode | 0x5;
+    } else if(strcmp(ir,"POP")){
+      opcode = opcode | 0x6;
+    } else if(strcmp(ir, "BRZ")){
+      opcode = opcode | 0x7;
+    } else {
+      printf("Bad instruction at %d\n",i+1);
+    }
+    opcode = opcode << 5;
+    
 
+    i++;
+  }
 
+}
